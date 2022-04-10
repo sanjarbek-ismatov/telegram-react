e46a70fd6765fdcd8d233900266a77b7bbeae30f
uzweb-code <ismatovsanjarbek@yandex.ru>
2022-04-10 20:52:30 +0500

    4 files modified, 1 added

--- src/layout/side.js
+++ src/layout/side.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faSearch } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
+import users from '../json/users';
 
 function Side() {
   return (
@@ -14,53 +16,17 @@ function Side() {
       </div>
 
       <div className="content-side">
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
+        {users.map((user) => {
+          return (
+            <div className="contact">
+              <img src={user.image} />
+              <div className="user-message">
+                <h3>{user.name}</h3>
+                <p>{user.message}</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
